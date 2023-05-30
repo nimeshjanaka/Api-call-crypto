00180be4c4d5d6549e2f7ad97dab45de93d0098e
nimeshjanaka <nimeshajanaka.1995@gmail.com>
2023-05-30 14:21:58 +0530

    card styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import MainLayout from './Layout';
+import { ConfigProvider, theme } from 'antd';
 
 
 const App = () => {
   
   return (
-    <MainLayout/>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm
+      }}
+    >
+      <MainLayout/>
+    </ConfigProvider>
   );
 };
 
