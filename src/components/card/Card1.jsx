ae5af69ba78e09847b5b6c871a791ea3203050b3
nimeshjanaka <nimeshajanaka.1995@gmail.com>
2023-05-30 14:48:45 +0530

    styles changes

--- src/components/card/Card1.jsx
+++ src/components/card/Card1.jsx
@@ -1,52 +1,44 @@
-import React from 'react'
-import './card1.css';
+import React from "react";
+import "./card1.css";
 
 const Card1 = () => {
   return (
-   <>
-   <div className='my-4 bg-primary rounded-x1 py-2'>
-    <div className='p1'>
-      <h2>Cross-Domain Arbitrage Tracker</h2>
-      <div className='p2'>
-      <p>Optimal arbitrage calculated for </p>
-      <span className='rounded-md p-1 bg-odosgray02 inline-block'>
-          "34,866,461" "paths"
-      </span>
-      
+    <>
+      <div className="my-4 rounded-x1 py-2" style={{ background: "#051c2c" }}>
+        <div>
+          <h2>Cross-Domain Arbitrage Tracker</h2>
+          <div className="d-flex">
+            <span className="my-auto">Optimal arbitrage calculated for</span>
+            <span className="rounded-md p-1 bg-odosgray02 fw-bold">
+              34,866,461 paths
+            </span>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
-    <div>
-    <div className='grid grid-cols-3 gap-2 mb-8'>
-      <div className='text-center bg-primary rounded-x1 py-2'>
-        <span className='text-white odos-subtitle-3'>
-          "34.4382"
-           "DAI"
-        </span>
-      </div>
-    </div>
-    <div className='grid grid-cols-3 gap-2 mb-8'>
-      <div className='text-center bg-primary rounded-x1 py-2'>
-        <span className='text-white odos-subtitle-3'>
-          "34.4382"
-           "DAI"
-        </span>
-      </div>
-    </div>
-    <div className='grid grid-cols-3 gap-2 mb-8'>
-      <div className='text-center bg-primary rounded-x1 py-2'>
-        <span className='text-white odos-subtitle-3'>
-          "34.4382"
-           "DAI"
-        </span>
-      </div>
-    </div>
+      <div className="d-flex">
+        <div
+          className="text-center rounded-x1 py-2 mt-2"
+          style={{ background: "#051c2c" }}
+        >
+          <span className="text-white odos-subtitle-3">Input <br />3.5679 LINK</span>
+        </div>
 
-    </div>
-    
-   
+        <div
+          className="text-center rounded-x1 py-2 mt-2"
+          style={{ background: "#051c2c" }}
+        >
+          <span className="text-white odos-subtitle-3">Output <br />3.5679 LINK</span>
+        </div>
+
+        <div
+          className="text-center rounded-x1 py-2 mt-2"
+          style={{ background: "#051c2c" }}
+        >
+          <span>Gross Profit <br />3.5679 LINK</span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Card1
+export default Card1;
